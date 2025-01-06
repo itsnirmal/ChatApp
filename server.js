531d5f15e0c1f48d3931719e4bf61d7beb778ad6
Nirmal Gautam <66936373+itsnirmal@users.noreply.github.com>
2025-01-05 23:49:15 -0600

    Changed cors url

--- server.js
+++ server.js
@@ -20,17 +20,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.error(`Blocked by CORS: ${origin}`);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
-    allowedHeaders: ['Content-Type', 'Authorization'],     
+    origin: allowedOrigins,
+    credentials: true
 }));
 
 app.options('*', cors());
